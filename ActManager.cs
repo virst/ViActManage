d1b73fd312fed40acd9ee27a38f61976f406d4e4
Вирстюк Михаил <m.vi@live.com>
2021-06-25 16:08:05 +0500

    initial commit

---
+++ ActManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViActManage
+{
+    public static class ActManager
+    {
+        public static bool Retry(this Action act, int retryCount = 5)
+        {
+            for (int i = 0; i < retryCount || retryCount == 0; i++)
+            {
+                try
+                {
+                    act.Invoke();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ViActManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+                ActManager.Retry(() =>
+                {
+                    Console.Write("*");
+                    int n = i - 5;
+                    Console.Write(Math.Round((double)(4 / n)));
+                });
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
